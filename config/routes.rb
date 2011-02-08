345ae1a860153a20974c81c6ba712ce8b75edf36
David Roetzel <david@roetzel.de>
2011-02-08 18:55:07 +0100

    Added home controller stub.

--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,7 @@ Nab::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
