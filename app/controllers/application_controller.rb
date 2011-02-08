217d69cb07fee5dab76c523ac4c27b7c20ee7606
David Roetzel <david@roetzel.de>
2011-02-08 20:06:45 +0100

    Load conference, if available.
    And display in page headline.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_conference
+
+  protected
+
+  def load_conference
+    @conference = Conference.find(params[:conference_id]) if params[:conference_id]
+  end
+
 end
