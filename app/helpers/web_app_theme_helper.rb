9e43ee3b752abacbda75b92f37a44a50face065b
David Roetzel <david@roetzel.de>
2011-02-14 12:58:21 +0100

    "Web-app-themeized" views a little.

---
+++ app/helpers/web_app_theme_helper.rb
@@ -0,0 +1,18 @@
+module WebAppThemeHelper 
+
+  def block(&block)
+    content_tag(:div, {:class => "block"}, &block)
+  end
+
+  def content(&block)
+    content_tag(:div, {:class => "content"}, &block)
+  end
+
+  def inner(&block)
+    content_tag(:div, {:class => "inner"}, &block)
+  end
+
+  def actions_bar(&block)
+    content_tag(:div, {:class => "actions-bar"}, &block)
+  end
+end
