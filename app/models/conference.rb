b9eab3944814444da45195913d028b9836bd658a
David Roetzel <david@roetzel.de>
2011-03-16 16:07:14 +0100

    Improved rejection of nested rooms.

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -5,7 +5,7 @@ class Conference < ActiveRecord::Base
   has_many :rooms
   has_many :tracks
 
-  accepts_nested_attributes_for :rooms, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :rooms, :reject_if => proc {|r| r["name"].blank?}, :allow_destroy => true
   accepts_nested_attributes_for :tracks, :reject_if => :all_blank, :allow_destroy => true
 
   validates_presence_of :title, :acronym
