f882fdf1fa116c9768b86172db206e15d7dbaad4
David Roetzel <david@roetzel.de>
2011-03-16 17:30:30 +0100

    Enhanced cfp interface.

--- app/helpers/cfp/people_helper.rb
+++ app/helpers/cfp/people_helper.rb
@@ -1,2 +1,8 @@
 module Cfp::PeopleHelper
+
+  def cfp_hard_deadline_over?
+    return false unless @conference.call_for_papers.hard_deadline
+    Date.today > @conference.call_for_papers.hard_deadline
+  end
+
 end
