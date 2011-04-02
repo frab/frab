25488edea0b5bf794ffbbe0f8728c99dd155be22
David Roetzel <david@roetzel.de>
2011-04-02 13:05:06 +0200

    Workaround for password reset.

--- app/controllers/cfp/passwords_controller.rb
+++ app/controllers/cfp/passwords_controller.rb
@@ -2,4 +2,15 @@ class Cfp::PasswordsController < Devise::PasswordsController
 
   layout "signup"
 
+  before_filter :set_call_for_papers, :only => :create
+
+  private
+
+  def set_call_for_papers
+    user = User.find_by_email(params[:cfp_user][:email])
+    if user
+      user.update_attributes(:call_for_papers_id => @conference.call_for_papers.id)
+    end
+  end
+
 end
