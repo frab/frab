c97d2a9b774bb0671575944087d17f2531a83ab3
David Roetzel <david@roetzel.de>
2011-05-26 09:41:04 +0200

    Fixed event creation from admin area.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -68,6 +68,7 @@ class EventsController < ApplicationController
   # POST /events.xml
   def create
     @event = Event.new(params[:event])
+    @event.conference = @conference
 
     respond_to do |format|
       if @event.save
