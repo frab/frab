66d551520d97227e1773f93650a57349776b9f11
David Roetzel <david@roetzel.de>
2011-06-23 16:49:42 +0200

    Fix schedule display when no events exist.
    Should probably display a nice message, though.

--- app/helpers/public/schedule_helper.rb
+++ app/helpers/public/schedule_helper.rb
@@ -1,8 +1,8 @@
 module Public::ScheduleHelper
 
   def each_time_slot_of(day, &block)
-    time = @conference.events.public.accepted.scheduled_on(day).order(:start_time).first.start_time 
-    while (time < day.end_of_day)
+    time = @conference.events.public.accepted.scheduled_on(day).order(:start_time).first.try(:start_time)
+    while (time and time < day.end_of_day)
       yield time
       time = time.since(@conference.timeslot_duration.minutes)
     end
