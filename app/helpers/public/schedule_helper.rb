74552d1c3e75f989e36332b47422451a43d1a4a8
David Roetzel <david@roetzel.de>
2011-08-05 18:10:14 +0200

    Added day start and day end to conferences.
    Schedules will be rendered using those times.

--- app/helpers/public/schedule_helper.rb
+++ app/helpers/public/schedule_helper.rb
@@ -1,8 +1,8 @@
 module Public::ScheduleHelper
 
   def each_time_slot_of(day, &block)
-    time = @conference.events.public.accepted.scheduled_on(day).order(:start_time).first.try(:start_time)
-    while (time and time < day.end_of_day)
+    time = @conference.day_start
+    while (time < @conference.day_end)
       yield time
       time = time.since(@conference.timeslot_duration.minutes)
     end
