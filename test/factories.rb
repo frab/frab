cdc8dfd672a456f09493fb72ba040726787d6a65
David Roetzel <david@roetzel.de>
2011-09-02 17:45:30 +0200

    Unit tests for availabilities.

--- test/factories.rb
+++ test/factories.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     "test#{n}@example.com"
   end
 
-  sequence :conference_acronyms do |n|
+  sequence :conference_acronym do |n|
     "frabcon#{n}"
   end
 
@@ -27,14 +27,22 @@ FactoryGirl.define do
     timeslot_duration 15
     default_timeslots 4
     max_timeslots 20
-    first_day { Date.today.since(60.days) }
-    last_day { Date.today.since(62.days) }
+    first_day { Date.today.since(60.days).to_date }
+    last_day { Date.today.since(62.days).to_date }
   end
 
   factory :call_for_papers do
     start_date { Date.today.ago(1.days) }
     end_date { Date.today.since(6.days) }
     conference
+  end
+
+  factory :availability do
+    conference
+    person
+    day { conference.days.first }
+    start_time "08:00"
+    end_time "20:00"
   end
 
 end
--- test/unit/availability_test.rb
+++ test/unit/availability_test.rb
@@ -1,8 +1,35 @@
 require 'test_helper'
 
 class AvailabilityTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  setup do
+    @conference = FactoryGirl.create(:conference)
   end
+
+  test "build_for creates availabilites for each conference day" do
+    availabilities = Availability.build_for(@conference)
+    assert_equal @conference.days.size, availabilities.size
+  end
+
+  test "time_range can be set and read successfully" do
+    availability = FactoryGirl.build(:availability, :conference => @conference)
+    availability.time_range = "7-23"
+    assert_equal "7-23", availability.time_range
+  end
+
+  test "correctly determines if given time is within current range" do
+    availability = FactoryGirl.build(:availability, :conference => @conference)
+    availability.time_range = "10-12"
+    time = Time.parse("09:00:00")
+    assert !availability.within_range?(time)
+    time = time.since(1.hours)
+    assert availability.within_range?(time)
+    time = time.since(1.hours)
+    assert availability.within_range?(time)
+    time = time.since(1.hours)
+    assert availability.within_range?(time)
+    time = time.since(1.hours)
+    assert !availability.within_range?(time)
+  end
+
 end
