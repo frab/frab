5566694b7118ed59c9a4af3a3bd0cb45e4bff578
David Roetzel <david@roetzel.de>
2011-10-18 10:50:55 +0200

    Use conference email as reply-to instead of from.
    This way you can configure a single from-address in
    settings.yml and make sure this is set up correctly
    to not have the emails end up in spam folders. And at
    the same time you can still redirect answers to the
    proper destination on a per-conference basis.

--- app/mailers/selection_notification.rb
+++ app/mailers/selection_notification.rb
@@ -1,5 +1,7 @@
 class SelectionNotification < ActionMailer::Base
  
+  default :from => Settings['from_email']
+  
   def acceptance_notification(event_person)
     @person = event_person.person
     @event = event_person.event
@@ -7,7 +9,7 @@ class SelectionNotification < ActionMailer::Base
     @locale = @person.locale_for_mailing(@conference)
     @token = event_person.confirmation_token
     mail(
-      :from => @conference.email,
+      :reply_to => @conference.email,
       :to => event_person.person.email,
       :subject => t("emails.event_acceptance.subject", :locale => @locale, :title => @conference.title)
     )
@@ -19,7 +21,7 @@ class SelectionNotification < ActionMailer::Base
     @conference = @event.conference
     @locale = @person.locale_for_mailing(@conference)
     mail(
-      :from => @conference.email,
+      :reply_to => @conference.email,
       :to => @person.email,
       :subject => t("emails.event_rejection.subject", :locale => @locale, :title => @conference.title)
     )
