f036ddc08fea362390ae81b317c1f4414e8a67ad
David Roetzel <david@roetzel.de>
2011-10-18 18:15:31 +0200

    Precompile some missing assets in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Frab::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile += ['admin.css', 'admin.js', 'schedule.js']
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
