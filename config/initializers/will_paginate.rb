9949b14f39106a36ed5df9c054df1a79acee3c7b
David Roetzel <david@roetzel.de>
2011-11-30 11:52:53 +0100

    Made will_paginate play nice with bootstrap.

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,29 @@
+module WillPaginate
+  module ActionView
+    def will_paginate(collection = nil, options = {})
+      options[:renderer] ||= BootstrapLinkRenderer
+      super.try :html_safe
+    end
+
+    class BootstrapLinkRenderer < LinkRenderer
+      protected
+
+      def html_container(html)
+        tag :div, tag(:ul, html), container_attributes
+      end
+
+      def page_number(page)
+        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+      end
+
+      def previous_or_next_page(page, text, classname)
+        tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+      end
+
+      def gap
+        tag :li, link(super, '#'), :class => 'disabled'
+      end
+
+    end
+  end
+end
