2a858c5758f67c67cac9fd03cf7169410e028866
Mario Manno <mm@koeln.ccc.de>
2012-01-10 06:31:44 +0100

    password reset, mail verification mails get i18n subjects
    Conflicts:
    	app/mailers/user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,12 @@ class UserMailer < ActionMailer::Base
 
   def password_reset_instructions(user)
     @user = user
-    mail :to => @user.email, :subject => I18n.t(:password_reset_instructions)
+    mail :to => @user.email, :subject => I18n.t("mailers.user_mailer.password_reset_instructions")
   end
 
   def confirmation_instructions(user)
     @user = user
-    mail :to => @user.email, :subject => I18n.t(:confirmation_instructions)
+    mail :to => @user.email, :subject => I18n.t("mailers.user_mailer.confirmation_instructions")
   end
 
 end
