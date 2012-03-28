59dd0d2ec64b808cf70bab9b8fc877913950daf0
Mario Manno <mm@koeln.ccc.de>
2012-03-28 15:01:24 +0200

    report controller first working version

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,24 +6,52 @@ class ReportsController < ApplicationController
   def index
   end
 
-  def show_people
-    @id = params[:id]
-    @people = []
+  def show_events
+    @report_type = params[:id]
+    @events = []
+    @search_count = 0
 
-    if @report_id
-      @search = Person.search(params[:q])
-      @people = @search.result.paginate :page => params[:page]
+    conference_events = @conference.events
+    if params[:term]
+        conference_events = @conference.events.with_query(params[:term])
+    end
+
+    case @report_type
+    when 'lectures_with_speaker'
+      r = conference_events.with_speaker.where(:event_type => :lecture)
+    when 'events_without_speaker'
+      r = conference_events.without_speaker
+    when 'events_with__speakers'
+      r = conference_events.joins(:event_people).where(:event_people => { :role_state => [:canceled, :declined, :idea, :offer, :unclear], :event_role => :speaker } )
+    end
+
+    unless r.nil?
+      @search = r.search(params[:q])
+      @search_count = r.count
+      @events = @search.result.paginate :page => params[:page]
     end
     render :show
   end
 
-  def show_events
-    @report_id = params[:id]
-    @events = []
+  def show_people
+    @report_type = params[:id]
+    @people = []
+    @search_count = 0
 
-    if @report_id
-      @search = @conference.events.search(params[:q])
-      @events = @search.result.paginate :page => params[:page]
+    conference_people = Person
+    if params[:term]
+        conference_people = Person.with_query(params[:term])
+    end
+
+    case @report_type
+    when 'people_speaking_at'
+      r = conference_people.speaking_at
+    end
+
+    unless r.nil?
+      @search = r.search(params[:q])
+      @search_count = r.count
+      @people = @search.result.paginate :page => params[:page]
     end
     render :show
   end
