4566a8872f937d855b8d6ae6738c65ec74aa08fb
Mario Manno <mm@koeln.ccc.de>
2012-04-25 16:56:05 +0200

    converted to integer times

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -8,32 +8,39 @@ module ScheduleHelper
     @rooms.size > 3
   end
 
+  # for pdf
   def number_of_rows
-    ((@conference.day_end - @conference.day_start) / 900).to_i
+    ((@conference.day_end - @conference.day_start) * 60 / @conference.timeslot_duration).to_i
   end
  
+  # for pdf
   def number_of_timeslots
     number_of_rows * 15.0 / @conference.timeslot_duration
   end
 
+  # for event boxes in public schedule
   def event_coordinates(room_index, event, column_width, row_height, offset = 0)
     x = 1.5.cm - 1 + room_index * column_width
-    day_end = event.start_time.change(:hour => @conference.day_end.hour, :min => @conference.day_end.min)
+    day_end = event.start_time.change(:hour => @conference.day_end, :min => 0)
     y = ((day_end - event.start_time) / (@conference.timeslot_duration * 60)) * row_height
     y += offset
     [x, y]
   end
 
   def each_minutes(minutes, &block)
-    time = @conference.day_start
-    while (time < @conference.day_end)
+    time = @conference.day_start*60
+    while time < @conference.day_end*60
       yield time
-      time = time.since(minutes.minutes)
+      time += minutes
     end
   end
 
   def each_15_minutes(&block)
     each_minutes(15, &block)
+  end
+
+  def minutes_to_time_str(minutes)
+    "#{minutes/60}:" + "%02d" % (minutes%60)
   end
 
 end
