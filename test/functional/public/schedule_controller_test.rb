df374313a012fb7d49d1da341106e5e07e3a6103
David Roetzel <david@roetzel.de>
2012-06-15 11:24:40 +0200

    Add (very) basic tests for public schedule.

--- test/functional/public/schedule_controller_test.rb
+++ test/functional/public/schedule_controller_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class Public::ScheduleControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  setup do
+    @conference = FactoryGirl.create(:conference)
+    10.times do
+      FactoryGirl.create(:event, :conference => @conference, :state => "confirmed")
+    end
   end
+
+  test "displays schedule main page" do
+    get :index, :conference_acronym => @conference.acronym
+    assert_response :success
+  end
+
+  test "displays xml schedule" do
+    get :index, :format => :xml, :conference_acronym => @conference.acronym
+    assert_response :success
+  end
+
+  test "displays json schedule" do
+    get :index, :format => :json, :conference_acronym => @conference.acronym
+    assert_response :success
+  end
+
+  test "displays ical schedule" do
+    get :index, :format => :ics, :conference_acronym => @conference.acronym
+    assert_response :success
+  end
+
+  test "displays xcal schedule" do
+    get :index, :format => :xcal, :conference_acronym => @conference.acronym
+    assert_response :success
+  end
+
 end
