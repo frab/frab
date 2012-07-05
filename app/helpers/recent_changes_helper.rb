bb26768c917f1df1e3db94836ecc72076babca48
David Roetzel <david@roetzel.de>
2012-07-05 11:50:06 +0200

    Make display of recent changes more robust.
    Deleting events with associated people led to exceptions
    when displaying the home and recent changes pages.
    
    This fixes this and possibly other conditions where the
    data that should be displayed has been deleted.

--- app/helpers/recent_changes_helper.rb
+++ app/helpers/recent_changes_helper.rb
@@ -8,6 +8,8 @@ module RecentChangesHelper
       associated = version.associated_type.constantize.find(version.associated_id)
       link_to associated.to_s, associated
     end
+  rescue ActiveRecord::RecordNotFound
+    version.associated_type
   end
 
   def verb_for(event)
