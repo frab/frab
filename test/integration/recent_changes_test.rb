7842db7ccc30a947dd41b4012b4af58a02f45115
Mario Manno <mm@koeln.ccc.de>
2012-07-22 20:01:23 +0200

    recent changes test checks for deleted users

--- test/integration/recent_changes_test.rb
+++ test/integration/recent_changes_test.rb
@@ -4,8 +4,9 @@ class RecentChangesTest < ActionDispatch::IntegrationTest
 
   setup do
     @conference = create(:conference)
-    user = create(:user, :person => create(:person), :role => "admin")
-    post "/session", :user => {:email => user.email, :password => "frab23"}
+    @user = create(:user, :person => create(:person), :role => "admin")
+    @tmp_user = create(:user, :person => create(:person), :role => "admin")
+    post "/session", :user => {:email => @user.email, :password => "frab23"}
   end
 
   teardown do
@@ -23,4 +24,14 @@ class RecentChangesTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "home page still displays after initiator of change has been deleted" do
+    post "/session", :user => {:email => @tmp_user.email, :password => "frab23"}
+    PaperTrail.enabled = true
+    event = create(:event, :conference => @conference)
+    event_person = create(:event_person, :event => event)
+    delete "/#{@conference.acronym}/people/#{@tmp_user.id}"
+    post "/session", :user => {:email => @user.email, :password => "frab23"}
+    get "/", :conference_acronym => @conference.acronym
+    assert_response :success
+  end
 end
