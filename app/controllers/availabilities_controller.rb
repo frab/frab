879d312d5c047658e2bb6c12ae1750cb358cead7
mm <mm@koeln.ccc.de>
2012-08-11 14:45:38 +0200

    availabilities slider form reworked

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -11,9 +11,24 @@ class AvailabilitiesController < ApplicationController
 
   def edit
     @availabilities = @person.availabilities_in(@conference)
+    @availabilities.each { |a|
+      a.start_date = a.start_date.in_time_zone
+      a.end_date = a.end_date.in_time_zone
+    }
   end
 
   def update
+    # remove empty availabilities
+    params[:person]['availabilities_attributes'].each { |k,v| 
+      Availability.delete(v['id']) if v['start_date'].to_i == -1
+    }
+    params[:person]['availabilities_attributes'].select! { |k,v| v['start_date'].to_i > 0 }
+    # fix dates
+    params[:person]['availabilities_attributes'].each { |k,v|
+      v['start_date']  = Time.zone.parse(v['start_date'])
+      v['end_date']  = Time.zone.parse(v['end_date'])
+    }
+
     @person.update_attributes(params[:person])
     redirect_to(person_url(@person), :notice => 'Availibility was successfully updated.')
   end
--- app/models/availability.rb
+++ app/models/availability.rb
@@ -7,7 +7,8 @@ class Availability < ActiveRecord::Base
   belongs_to :conference
   belongs_to :day
 
-  validate :start_date_before_end_date
+  validate :dates_valid?
+  validate :start_date_before_end_date?
   after_save :update_event_conflicts
 
   def self.build_for(conference)
@@ -45,8 +46,18 @@ class Availability < ActiveRecord::Base
     end
   end
   
-  def start_date_before_end_date
+  def start_date_before_end_date?
     self.errors.add(:end_date, "should be after start date") if self.end_date < self.start_date
+  end
+
+  def year_valid?(year)
+    return false if year < 1990 or year > 2100
+    return true
+  end
+
+  def dates_valid?
+    self.errors.add(:start_date, "not a valid date") unless year_valid?(self.start_date.year)
+    self.errors.add(:end_date, "not a valid date") unless year_valid?(self.end_date.year)
   end
 
 end
