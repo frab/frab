dd62654e14d3ba6f2c0d3815992871479cf15a20
Matthias Schiffer <mschiffer@universe-factory.net>
2012-10-24 01:29:27 +0200

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120806161457) do
+ActiveRecord::Schema.define(:version => 20120920213816) do
 
   create_table "availabilities", :force => true do |t|
     t.integer  "person_id"
@@ -160,14 +160,6 @@ ActiveRecord::Schema.define(:version => 20120806161457) do
     t.datetime "updated_at"
   end
 
-  create_table "ordered_products", :force => true do |t|
-    t.integer  "attendee_id"
-    t.integer  "product_type_id"
-    t.integer  "amount"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "people", :force => true do |t|
     t.string   "first_name",          :default => ""
     t.string   "last_name",           :default => ""
@@ -194,21 +186,6 @@ ActiveRecord::Schema.define(:version => 20120806161457) do
     t.string   "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "product_types", :force => true do |t|
-    t.string   "type"
-    t.integer  "attendee_registration_id"
-    t.string   "name"
-    t.decimal  "price",                    :precision => 7, :scale => 2
-    t.decimal  "vat",                      :precision => 4, :scale => 2
-    t.integer  "includes_vat"
-    t.date     "available_until"
-    t.integer  "amount_available"
-    t.integer  "needs_invoice_address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "currency"
   end
 
   create_table "rooms", :force => true do |t|
