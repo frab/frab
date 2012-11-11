036f6f301f1e157743605d6d7f7d0947eb29dfc5
Mario Manno <mail@manno.name>
2012-11-11 21:44:27 +0000

    enable nginx config options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,10 @@ Frab::Application.configure do
   config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
