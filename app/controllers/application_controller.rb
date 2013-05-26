7093b88a825323b6e140c2592c14d7aaba9a255c
Mario Manno <mm@koeln.ccc.de>
2013-05-26 12:22:02 +0200

    if a submitter stumbled into our conference management namespace, redirect them properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |ex|
     Rails.logger.info "[ !!! ] Access Denied for #{current_user.email}/#{current_user.id}/#{current_user.role}: #{ex.message}" 
     begin
-      redirect_to :back, :notice => t(:"ability.denied")
+      if current_user.role == "submitter"
+        redirect_to cfp_root_path, :notice => t(:"ability.denied")
+      else
+        redirect_to :back, :notice => t(:"ability.denied")
+      end
     rescue ActionController::RedirectBackError
       redirect_to root_path
     end
