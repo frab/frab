f14fb92184a0506d0ba55f980bc63345918ca880
Mario Manno <mm@koeln.ccc.de>
2013-05-27 01:41:54 +0200

    moved vagrant/puppet based configuration into another branch

--- puppet/modules/rvm/manifests/dependencies.pp
+++
@@ -1,7 +0,0 @@
-class rvm::dependencies {
-  case $::operatingsystem {
-    Ubuntu,Debian: { require rvm::dependencies::ubuntu }
-    CentOS,RedHat: { require rvm::dependencies::centos }
-    OracleLinux,RedHat: { require rvm::dependencies::oraclelinux }
-  }
-}
--- puppet/modules/rvm/manifests/passenger/gem.pp
+++
@@ -1,7 +0,0 @@
-class rvm::passenger::gem($ruby_version, $version) {
-  rvm_gem {
-    "passenger":
-      ensure       => $version,
-      ruby_version => $ruby_version;
-  }
-}
