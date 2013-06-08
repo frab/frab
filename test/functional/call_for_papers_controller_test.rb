12e56e00048cb775847920731d8ccbd5e899e75e
Mario Manno <mm@koeln.ccc.de>
2013-06-08 15:52:01 +0200

    adds notification test

---
+++ test/functional/call_for_papers_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class CallForPapersControllerTest < ActionController::TestCase
+
+  setup do
+    @call_for_papers = FactoryGirl.create(:call_for_papers)
+    @conference = @call_for_papers.conference
+    login_as(:admin)
+  end
+
+  test "should show cfp" do
+    get :show, conference_acronym: @conference.acronym
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new, conference_acronym: @conference.acronym
+    assert_response :success
+  end
+
+  test "should create cfp" do
+    new_conference = FactoryGirl.create(:conference)
+    assert_difference('CallForPapers.count') do
+      call_for_papers = FactoryGirl.build(:call_for_papers, conference: new_conference)
+      post :create, call_for_papers: call_for_papers.attributes, conference_acronym: new_conference.acronym
+    end
+  end
+
+  test "should get edit" do
+    get :edit, conference_acronym: @conference.acronym
+    assert_response :success
+  end
+
+  test "should update cfp" do
+    put :update, call_for_papers: @call_for_papers.attributes.merge(welcome_text: "welcome"), conference_acronym: @conference.acronym
+    assert_redirected_to call_for_papers_path(conference_acronym: @conference.acronym)
+  end
+
+  test "should get edit notification" do
+    get :edit_notification, conference_acronym: @conference.acronym
+    assert_response :success
+  end
+
+  test "should add cfp notification" do
+    assert_difference('Notification.count') do
+      @call_for_papers.notification = FactoryGirl.create(:notification)
+      put :update, call_for_papers: @call_for_papers.attributes, conference_acronym: @conference.acronym
+    end
+  end
+
+end
