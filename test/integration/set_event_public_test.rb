8d06afefe5216b21785f8ba39c7245922c972d8c
Mario Manno <mm@koeln.ccc.de>
2013-06-15 18:03:12 +0200

    adds test for checkboxes on event show and edit page

---
+++ test/integration/set_event_public_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class SetEventPublicTest < ActionDispatch::IntegrationTest
+  setup do
+    @conference = create :three_day_conference
+    @event = create :event, conference: @conference
+    @user = create :orga_user
+    post "/session", user: {email: @user.email, password: "frab23"}
+  end
+
+  test "can set event to public" do
+    @event.public = true
+    put "/#{@conference.acronym}/events/#{@event.id}", event: @event.attributes
+    assert_redirected_to event_path(assigns(:event))
+
+    get "/#{@conference.acronym}/events/#{@event.id}"
+    assert_select '#public' do
+      assert_select 'input[checked]', true
+    end
+
+    get "/#{@conference.acronym}/events/#{@event.id}/edit"
+    assert_select '#event_public' do
+      assert_select 'input[checked]', true
+    end
+  end
+
+  test "can set event to private" do
+    @event.public = false
+    put "/#{@conference.acronym}/events/#{@event.id}", event: @event.attributes
+    assert_redirected_to event_path(assigns(:event))
+
+    get "/#{@conference.acronym}/events/#{@event.id}"
+    assert_select '#public' do
+      assert_select 'input[checked]', false
+    end
+
+    get "/#{@conference.acronym}/events/#{@event.id}/edit"
+    assert_select '#event_public' do
+      assert_select 'input[checked]', false
+    end
+  end
+
+end
