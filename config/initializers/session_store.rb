015d1f219f9ec391c383905389326b23fd610d23
Mario Manno <mm@koeln.ccc.de>
2013-07-02 03:39:39 +0200

    adds an example for a cookie store with secure cookie in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,10 @@
 
 Frab::Application.config.session_store :active_record_store, key: '_frab_session'
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Frab::Application.config.session_store :active_record_store
+# Example for a cookie store, with secure flag set for SSL hosting in production mode
+#
+# Frab::Application.config.session_store :cookie_store,
+#                                       key: '_frab_session',
+#                                       secure: Rails.env == 'production',
+#                                       httponly: true,
+#                                       expire_after: 60.minutes 
