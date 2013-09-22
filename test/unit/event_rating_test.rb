fbd216badd660fc07de71fa693856c484479077d
Mario Manno <mm@koeln.ccc.de>
2013-09-23 00:24:33 +0200

    fixes event ratings test

--- test/unit/event_rating_test.rb
+++ test/unit/event_rating_test.rb
@@ -4,15 +4,18 @@ class EventRatingTest < ActiveSupport::TestCase
 
   test "creating ratings correctly updates average" do
     event = FactoryGirl.create(:event)
+    id = event.id
+
+    FactoryGirl.create(:event_rating, event: event, rating: 8.0)
+    assert_equal 8.0, Event.find(id).average_rating
+
     FactoryGirl.create(:event_rating, event: event, rating: 4.0)
-    event.reload
-    assert_equal 4.0, event.average_rating
-    FactoryGirl.create(:event_rating, event: event, rating: 4.0)
-    event.reload
-    assert_equal 4.0, event.average_rating
-    FactoryGirl.create(:event_rating, event: event, rating: 1.0)
-    event.reload
-    assert_equal 3.0, event.average_rating
+    update_average(id)
+    assert_equal 6.0, Event.find(id).average_rating
+
+    FactoryGirl.create(:event_rating, event: event, rating: 3.0)
+    update_average(id)
+    assert_equal 5.0, Event.find(id).average_rating
   end
 
   test "person can only submit one rating per event" do
@@ -26,4 +29,11 @@ class EventRatingTest < ActiveSupport::TestCase
     assert !er.save
   end
 
+  private
+
+  def update_average(id)
+    event = Event.find(id)
+    event.recalculate_average_rating!
+    event.save!
+  end
 end
