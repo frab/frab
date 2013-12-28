5b4ab586bd838460521413ab0421fc8db819f70f
Mario Manno <mm@koeln.ccc.de>
2013-12-28 19:31:24 +0100

    increase attachment size to 42mb

--- app/models/event_attachment.rb
+++ app/models/event_attachment.rb
@@ -4,7 +4,7 @@ class EventAttachment < ActiveRecord::Base
 
   has_attached_file :attachment
 
-  validates_attachment_size :attachment, :less_than => 23.megabytes
+  validates_attachment_size :attachment, :less_than => 42.megabytes
 
   has_paper_trail meta: {associated_id: :event_id, associated_type: "Event"}
 
