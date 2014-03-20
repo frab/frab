5e588b672bd4ed16043deffdd2b45a7206183dac
Mario Manno <mm@koeln.ccc.de>
2014-03-20 20:34:22 +0100

    refs #124: add redirect to valid conference home for crew members

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,11 @@ class HomeController < ApplicationController
       return redirect_to new_conference_path
     end
     if cannot? :read, Conference
+      # maybe a crew user tries to login to the wrong conference?
+      if current_user.is_crew?
+        conference = current_user.conference_users.map { |cu| cu.conference }.last
+        return redirect_to conference_home_path(conference_acronym: conference.acronym) if conference.present?
+      end
       return redirect_to cfp_root_path
     end
     @versions = Version.where(conference_id: @conference.id).includes(:item).order("created_at DESC").limit(5)
