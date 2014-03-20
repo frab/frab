7ed08048d9d1036dceb2893970b8255c2d5ad9c6
Mario Manno <mm@koeln.ccc.de>
2014-03-20 14:38:34 +0100

    closes #64: only check the right conferences availabilities

--- app/models/event_person.rb
+++ app/models/event_person.rb
@@ -35,8 +35,10 @@ class EventPerson < ActiveRecord::Base
 
   def available_between?(start_time, end_time)
     return unless start_time and end_time
-    self.person.availabilities.any? { |a| a.within_range? (start_time) and
-                                         a.within_range? (end_time) }
+    conference = self.event.conference
+    availabilities = self.person.availabilities_in(conference)
+    availabilities.any? { |a| a.within_range? (start_time) and 
+                          a.within_range? (end_time) }
   end
 
   def to_s
