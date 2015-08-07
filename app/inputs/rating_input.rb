308a1c53bd9d5ca07ff559307da7944368097423
Mario Manno <mail@manno.name>
2015-08-07 21:28:25 +0200

    remove event_ratings link_to_function

--- app/inputs/rating_input.rb
+++ app/inputs/rating_input.rb
@@ -1,7 +1,5 @@
 class RatingInput < FormtasticBootstrap::Inputs::HiddenInput
   def to_html
-    generic_input_wrapping do
-      builder.hidden_field(method, input_html_options) + "<div id=\"my_rating\"></div>".html_safe
-    end
+    builder.hidden_field(method, input_html_options) + "<div id=\"my_rating\"></div>".html_safe
   end
 end
