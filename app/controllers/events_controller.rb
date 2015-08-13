ebc3feffdb763a62977fa85afcfdb77ead39145c
Mario Manno <mail@manno.name>
2015-08-13 18:44:57 +0200

    fix strong_params for attachment in events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -239,7 +239,7 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(
       :id, :title, :subtitle, :event_type, :time_slots, :state, :start_time, :public, :language, :abstract, :description, :logo, :track_id, :room_id, :note, :submission_note, :do_not_record, :recording_license,
-      event_attachments_attributes: %i(id title public _destroy),
+      event_attachments_attributes: %i(id title attachment public _destroy),
       ticket_attributes: %i(id remote_ticket_id),
       links_attributes: %i(id title url _destroy),
       event_people_attributes: %i(id person_id event_role role_state _destroy)
