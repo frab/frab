bdefb4e3215672fb3a5f003fb5f6744c1da2cf45
Mario Manno <mail@manno.name>
2015-09-12 21:47:46 +0200

    fix notification migration

--- db/migrate/20150815181454_move_notifications_onto_conference.rb
+++ db/migrate/20150815181454_move_notifications_onto_conference.rb
@@ -2,8 +2,8 @@ class MoveNotificationsOntoConference < ActiveRecord::Migration
   def change
     add_column :notifications, :conference_id, :integer
     Notification.find_each { |n|
-      next unless n.call_for_papers
-      n.update(conference_id: n.call_for_papers.conference.id)
+      next unless n.call_for_papers_id
+      execute %(UPDATE notifications SET conference_id=(SELECT conference_id FROM call_for_papers AS cfp WHERE cfp.id=#{n.call_for_papers_id}) WHERE id=#{n.id};)
     }
     remove_column :notifications, :call_for_papers_id
   end
