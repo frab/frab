95d1bc003310b0f66fa358321bcbf7844cf1a57f
Mario Manno <mail@manno.name>
2015-09-12 17:43:38 +0200

    Revert "re-add load balancer monkey patch to avoid trace page in production (rails3?)"
    This reverts commit 6c664d94706c3f0b81c58ac898e1de4b25237d98.

--- config/initializers/load_balancers.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.production?
-  # Don't show trace pages in production! Requests shall never originate from localhost!
-  # see: http://helderribeiro.net/?p=357
-  class ActionDispatch::Request
-    def local?
-      false
-    end
-  end
-end
