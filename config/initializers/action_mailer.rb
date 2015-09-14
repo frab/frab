f62bdc59cbfd41e2e7e617258f2c914217a12fea
Mario Manno <mail@manno.name>
2015-09-14 16:12:38 +0200

    add config option to disable ssl/tls for smtp

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -4,7 +4,14 @@ ActionMailer::Base.default_url_options = {
 }
 if ENV["SMTP_ADDRESS"]
   %w(ADDRESS PORT DOMAIN USER_NAME PASSWORD).each do |setting|
-    next unless ENV["SMTP_#{setting}"]
+    next unless ENV["SMTP_#{setting}"].present?
     ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"]
   end
+end
+if ENV["SMTP_NOTLS"].present?
+  ActionMailer::Base.smtp_settings.merge!(
+    enable_starttls_auto: false,
+    ssl: false,
+    tls: false
+  )
 end
