60545ee38ca89f9a77508d6d1138a27f311f3f6d
Mario Manno <mail@manno.name>
2016-04-15 00:48:35 +0200

    add vendor image folders

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,10 +5,14 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'colorpicker')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'icons')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'raty')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [
   'admin.css', 'admin.js', 'schedule.js', 'public_schedule.css', 'public_schedule_print.css', 'public_schedule.js', 'moment.min.js',
-  /\.(?:jpg|png|gif)/ # TODO images in vendor dir?
+  /\.(?:jpg|png|gif)/
 ]
