325fe751f57b303808005de18dcd70ad51ac303a
Mario Manno <mail@manno.name>
2016-05-15 09:42:17 +0200

    fix toggle description javascript on event rating page

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -2,4 +2,8 @@ $(function() {
   $('.topbar').dropdown();
   $('.alert-message').alert();
   $('a[data-original-title]').popover();
+  $('[data-function="toggle"]').click(function(){
+    var args = $(this).data("args");
+    $(args.target).toggle();
+  });
 });
