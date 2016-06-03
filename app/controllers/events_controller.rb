e3af1c7d65badaeb0edcfaad12490498cfe505d3
Dirk Engling <erdgeist@erdgeist.org>
2016-06-03 10:10:16 +0000

    Rename state to event_state and pull the correct event_type parameter from params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -220,9 +220,9 @@ class EventsController < ApplicationController
 
   def search(events, params)
     filter = events
-    filter = filter.where( {state: params[:state]}) if params[:state].present?
-    filter = filter.where( {event_type: params[:type]}) if params[:type].present?
-    filter = filter.where( {track: Track.find_by(:name => params[:track_name])}) if params[:track_name].present?
+    filter = filter.where(state: params[:event_state]) if params[:event_state].present?
+    filter = filter.where(event_type: params[:event_type]) if params[:event_type].present?
+    filter = filter.where(track: Track.find_by(:name => params[:track_name])) if params[:track_name].present?
 
     if params.key?(:term) and not params[:term].empty?
       term = params[:term]
