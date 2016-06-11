051ffb247e631b18aa83ea61949684d2163a8544
Dirk Engling <erdgeist@erdgeist.org>
2016-06-11 00:07:52 +0000

    Replace handwritten rt communication by roust gem

--- lib/ticket_server_adapters/rt_ticket_server_adapter.rb
+++ lib/ticket_server_adapters/rt_ticket_server_adapter.rb
@@ -1,6 +1,6 @@
 class RTTicketServerAdapter < TicketServerAdapter
-  require 'uri'
-  require 'net/http'
+  require 'rubygems'
+  require 'roust'
 
   def get_ticket_view_url(remote_id)
     uri = URI.parse(@server.url)
@@ -9,65 +9,24 @@ class RTTicketServerAdapter < TicketServerAdapter
     uri.to_s
   end
 
-  #
-  # connect to a remote ticket system and return remote_id
-  #
   def create_remote_ticket(args)
-    data = <<-EOF
-id: ticket/new
-Queue: #{@server.queue}
-Subject:  #{args[:title]}
-Requestor: #{args[:owner_email]}
-    EOF
+    rt = Roust.new({
+            :server   => @server.url,
+            :username => @server.user,
+            :password => @server.password
+            }, {
+            'Referer' => @server.url
+            })
+    fail "RT Error" if not rt.authenticated?
 
-    args[:requestors].each do |r|
-      data << "Requestor: #{r[:name]} <#{r[:email]}>"
-    end
+    ticket = rt.ticket_create({
+            'Subject'    => args[:title],
+            'Queue'      => @server.queue,
+            'Owner'      => 'Nobody',
+            'Requestors' => args[:requestors].collect { |r| "#{r[:name]} <#{r[:email]}>" },
+            })
 
-    remote_ticket_id = create(data)
-    remote_ticket_id
-  end
-
-  private
-
-  def get_http(uri)
-    http = Net::HTTP.new(uri.host, uri.port)
-    if uri.is_a? URI::HTTPS
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
-    http
-  end
-
-  def create(data)
-    uri = URI.parse(@server.url)
-    user = URI.encode(@server.user)
-    password = URI.encode(@server.password)
-    uri.path += 'REST/1.0/ticket/new'
-
-    if @test_only
-      @logger.info uri.path
-      @logger.info "content => #{data}"
-      return
-    end
-
-    request = Net::HTTP::Post.new(uri.path)
-    request.set_form_data('user' => user, 'pass' => password, 'content' => data)
-    http = get_http(uri)
-    response = http.request(request)
-
-    case response
-    when Net::HTTPSuccess
-      if response.body.match(/200 Ok/) and m = response.body.match(/Ticket (\d+) created./)
-        return m[1]
-      else
-        @logger.info response.to_json
-        fail "RT Error: #{response.body}"
-      end
-    else
-      @logger.info response.to_json
-      fail "RT HTTP Error: #{response.error!}"
-    end
+    ticket['id']
   end
 
 end
