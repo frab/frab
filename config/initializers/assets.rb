27266dffd54fe940e07bf43d40ed8af82598c1ca
Mario Manno <mail@manno.name>
2016-07-27 01:33:23 +0200

    remove public schedule js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'im
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [
-  'admin.css', 'admin.js', 'schedule.js', 'public_schedule.css', 'public_schedule_print.css', 'public_schedule.js', 'moment.min.js',
+  'admin.css', 'admin.js', 'schedule.js', 'public_schedule.css', 'public_schedule_print.css', 'moment.min.js',
   /\.(?:jpg|png|gif)/
 ]
