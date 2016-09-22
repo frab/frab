d42f5023b36b9dab3d23c3e40bed5aed9a36c148
Dirk Engling <erdgeist@erdgeist.org>
2016-09-22 22:37:22 +0200

    Finally, save the state machine progression

--- app/jobs/send_bulk_ticket_job.rb
+++ app/jobs/send_bulk_ticket_job.rb
@@ -3,14 +3,13 @@ class SendBulkTicketJob
 
   def perform(conference, filter)
     Rails.logger.debug 'performing ' + filter + ' on ' + conference.acronym
-    return
     case filter
     when 'accepting'
-      conference.events.where(state: filter).map{|e| e.notify }
+      conference.events.where(state: filter).map{|e| e.notify! }
     when 'rejecting'
-      conference.events.where(state: filter).map{|e| e.notify }
+      conference.events.where(state: filter).map{|e| e.notify! }
     when 'confirmed'
-      conference.events.where(state: filter).scheduled.map{|e| e.notify }
+      conference.events.where(state: filter).scheduled.map{|e| e.notify! }
     end
 
   end
