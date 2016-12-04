030cd0afcb022e5906928ee3beaa1950f5bc8619
Mario Manno <mail@manno.name>
2016-12-04 21:28:57 +0100

    Clean up schedule controller day method, somewhat

--- app/controllers/public/schedule_controller.rb
+++ app/controllers/public/schedule_controller.rb
@@ -21,23 +21,13 @@ class Public::ScheduleController < ApplicationController
   end
 
   def day
-    @day_index = params[:day].to_i ||= 0
+    @day_index = params[:day].to_i
     if @conference.days.count <= @day_index
       return redirect_to public_schedule_index_path, alert: "Failed to find day for id #{@day_index}"
     end
-    @day = @conference.days[@day_index]
 
-    @all_rooms = @conference.rooms_including_subs
-    @rooms = []
-    @events = {}
-    @skip_row = {}
-    @all_rooms.each do |room|
-      events = room.events.confirmed.no_conflicts.is_public.scheduled_on(@day).order(:start_time).to_a
-      next if events.empty?
-      @events[room] = events
-      @skip_row[room] = 0
-      @rooms << room
-    end
+    setup_day_ivars
+
     if @rooms.empty?
       return redirect_to public_schedule_index_path, notice: 'No events are public and scheduled.'
     end
@@ -93,5 +83,21 @@ class Public::ScheduleController < ApplicationController
 
   def maybe_authenticate_user!
     authenticate_user! unless @conference.schedule_public
+  end
+
+  def setup_day_ivars
+    @day = @conference.days[@day_index]
+
+    all_rooms = @conference.rooms_including_subs
+    @rooms = []
+    @events = {}
+    @skip_row = {}
+    all_rooms.each do |room|
+      events = room.events.confirmed.no_conflicts.is_public.scheduled_on(@day).order(:start_time).to_a
+      next if events.empty?
+      @events[room] = events
+      @skip_row[room] = 0
+      @rooms << room
+    end
   end
 end
