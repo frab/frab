6f59a617bfa74b2e085bc08b12c01faf9d09fb60
Mario Manno <mail@manno.name>
2016-12-24 15:34:52 +0100

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923195521) do
+ActiveRecord::Schema.define(version: 20161224142836) do
 
   create_table "availabilities", force: :cascade do |t|
     t.integer  "person_id"
