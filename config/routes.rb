d2b60d90280bb38467a786d60e9db0357ff21bde
Mario Manno <mario.manno@gmail.com>
2017-03-30 00:17:58 -0700

    Use custom registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'auth/registrations'
+  }
 
   scope '(:locale)' do
 
--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -13,5 +13,13 @@ class SignUpTest < ActionDispatch::IntegrationTest
     get "/#{@conference.acronym}/cfp"
     assert_includes @response.body, "<title>#{@conference.title}"
     assert_includes @response.body, "#{@conference.title}\n- Call for Participation"
+    # get "/users/sign_up?conference_acronym=#{@conference.acronym}&locale=en"
+    post '/users', params: {
+      'user' => { 'email' => 'test2@example.org', 'password' => 'frab12345', 'password_confirmation' => 'frab12345' },
+      'commit' => 'Sign up', 'conference_acronym' => @conference.acronym, 'locale' => 'en'
+    }
+    follow_redirect!
+    assert_includes @response.body, 'confirmation link'
+    assert_includes @response.body, "#{@conference.title}\n- Call for Participation"
   end
 end
