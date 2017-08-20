17e365b77c89432e5289203ff3368ea909075bb0
Dirk Engling <erdgeist@erdgeist.org>
2017-08-20 23:43:37 +0200

    conference also must accept attributes for classifiers

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -20,6 +20,7 @@ class Conference < ApplicationRecord
   belongs_to :parent, class_name: 'Conference', optional: true
 
   accepts_nested_attributes_for :rooms, reject_if: proc { |r| r['name'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :classifiers, reject_if: proc { |r| r['name'].blank? }, allow_destroy: true
   accepts_nested_attributes_for :days, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :notifications, allow_destroy: true
   accepts_nested_attributes_for :tracks, reject_if: :all_blank, allow_destroy: true
