4c29b6278a274a64fdf18494a1b275e66d2aa3e8
Dirk Engling <erdgeist@erdgeist.org>
2017-09-09 01:02:26 +0200

    extend classifier test

--- test/models/classifier_test.rb
+++ test/models/classifier_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ClassifierTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to: :conference
+  should have_many: :event_classifiers
+
+  test "can create a classifier" do
+    conference = create(:conference)
+    classifier = create(:classifier, conference: conference)
+  end
 end
