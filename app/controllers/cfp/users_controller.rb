edffc38369df8d6d22fe4d6c3c91c8270af73482
Mario Manno <mario.manno@gmail.com>
2019-03-11 00:31:58 +0100

    CFP user can edit password despite `sign_in_after_reset_password` (closes #421)

--- app/controllers/cfp/users_controller.rb
+++ app/controllers/cfp/users_controller.rb
@@ -11,8 +11,11 @@ class Cfp::UsersController < ApplicationController
     [:password, :password_confirmation].each do |password_key|
       params[:user].delete(password_key) if params[:user][password_key].blank?
     end
+
     if @user.update_attributes(user_params)
-      redirect_to cfp_person_path(@user.person), notice: t(:"cfp.updated")
+      bypass_sign_in(@user) if Devise.sign_in_after_reset_password
+      redirect_to cfp_person_path, notice: t(:"cfp.updated")
+
     else
       flash_model_errors(@user)
       render action: 'edit'
