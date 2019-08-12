44bb5e249784d2bdfde9454741565a0880ea8a50
eladeyal-intel <elad.eyal@intel.com>
2019-08-12 23:30:20 +0300

    Add OmniAuth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,20 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  protect_from_forgery :except => [:failure]
+
+  def all
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+   
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication, :method => :post
+      set_flash_message(:notice, :success, :kind => t(action_name, scope: 'devise.links')) if is_navigational_format?
+    else
+      session['devise.'+action_name+'_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to root_path, alert: @user.errors.full_messages.join("\n") 
+    end
+  end
+  
+  def failure
+    set_flash_message(:notice, :unknown_failure) if is_navigational_format?
+    redirect_to root_path
+  end
+end
