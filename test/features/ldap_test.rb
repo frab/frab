08f01674d40f0825fe362b781333b7629d737927
eladeyal-intel <elad.eyal@intel.com>
2019-10-12 13:33:53 +0300

    Test for LDAP integration

---
+++ test/features/ldap_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class LdapTest < FeatureTest
+  LOGIN='tesla'     # see https://www.forumsys.com/category/tutorials/integration-how-to/
+  PASSWORD='password'
+  EMAIL='tesla@ldap.forumsys.com'
+
+  setup do
+    assert User.where(email: EMAIL).blank?
+  end
+
+  def connect_with_ldap
+    visit '/'
+   
+    click_on 'Log-in'
+   
+    click_on 'Sign in with free testing server at ldap.forumsys.com'
+   
+    fill_in 'Login:', with: LOGIN
+    fill_in 'Password:', with: PASSWORD
+    click_on 'Sign In'
+   
+    assert_content page, 'Successfully authenticated'
+    assert User.where(email: EMAIL).any?
+    
+    click_on 'Logout'
+  end
+
+  test 'can sign up and sign in with LDAP' do
+    connect_with_ldap # for new user
+    connect_with_ldap # for existing user
+  end
+  
+  teardown do
+    User.where(email: EMAIL).destroy_all
+  end
+end
