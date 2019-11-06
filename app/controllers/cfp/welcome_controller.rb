fa227e2052e9f0c6b8dcdb52d663935e6b0e2358
Dirk Engling <erdgeist-github@erdgeist.org>
2019-11-06 01:30:49 +0100

    There's a macro now to test a valid public_name

--- app/controllers/cfp/welcome_controller.rb
+++ app/controllers/cfp/welcome_controller.rb
@@ -2,7 +2,7 @@ class Cfp::WelcomeController < ApplicationController
   layout 'cfp'
   def show
     if current_user and session[:conference_acronym]
-      if current_user.person.public_name == current_user.email
+      if redirect_submitter_to_edit?
         flash[:alert] = t('users_module.error_invalid_public_name')
         redirect_to edit_cfp_person_path(conference_acronym: session[:conference_acronym])
       else
