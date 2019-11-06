9f67c0735c9f39a22fb8f2928b0eee9bd86582ac
erdgeist@erdgeist.org <erdgeist@erdgeist.org>
2019-11-06 21:30:27 +0100

    Silence devise warnings in tests

--- test/features/submit_event_test.rb
+++ test/features/submit_event_test.rb
@@ -9,8 +9,8 @@ class SubmitEventTest < FeatureTest
   def sign_up_steps
     click_on 'Sign Up', match: :first
     fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
-    fill_in 'Password confirmation', with: @user.password
+    fill_in 'user_password', with: @user.password
+    fill_in 'user_password_confirmation', with: @user.password
     click_on 'Sign up'
     assert_content page, 'A message with a confirmation link has been sent to your email address'
 
