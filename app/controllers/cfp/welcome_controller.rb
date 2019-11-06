a53f30a0533d3640dedc26a3a11f8212f365893c
Dirk Engling <erdgeist-github@erdgeist.org>
2019-11-06 01:28:47 +0100

    Don't redirect admin users, if they want to see the cfp interface

--- app/controllers/cfp/welcome_controller.rb
+++ app/controllers/cfp/welcome_controller.rb
@@ -2,14 +2,11 @@ class Cfp::WelcomeController < ApplicationController
   layout 'cfp'
   def show
     if current_user and session[:conference_acronym]
-      if @conference && policy(@conference).manage?
-        redirect_to conference_path(conference_acronym: session[:conference_acronym])
+      if current_user.person.public_name == current_user.email
+        flash[:alert] = t('users_module.error_invalid_public_name')
+        redirect_to edit_cfp_person_path(conference_acronym: session[:conference_acronym])
       else
-        if current_user.person.public_name == current_user.email
-          redirect_to edit_cfp_person_path(conference_acronym: session[:conference_acronym])
-        else
-          redirect_to cfp_person_path(conference_acronym: session[:conference_acronym])
-        end
+        redirect_to cfp_person_path(conference_acronym: session[:conference_acronym])
       end
     end
 
