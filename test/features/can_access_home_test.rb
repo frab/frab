51eadccc5f2dcf55430e87bd23330ac5478f7bfa
eladeyal-intel <elad.eyal@intel.com>
2019-11-16 03:25:22 +0200

    depracated (#649)

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 feature 'CanAccessHome' do
   scenario 'home page shows' do
     visit root_path
-    page.must_have_content 'frab'
+    assert_content page, 'frab'
   end
 
   scenario 'home page shows when js is enabled', js: true do
     visit root_path
-    page.must_have_content 'frab'
+    assert_content page, 'frab'
   end
 end
