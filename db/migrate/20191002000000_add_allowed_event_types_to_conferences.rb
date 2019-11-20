0236731e80d4c0b503d5dbe1339378cb7c1fca9a
Mario Manno <mario.manno@gmail.com>
2019-11-20 14:47:46 +0100

    fixup! configurable event types per conference

--- db/migrate/20191002000000_add_allowed_event_types_to_conferences.rb
+++ db/migrate/20191002000000_add_allowed_event_types_to_conferences.rb
@@ -1,5 +1,6 @@
 class AddAllowedEventTypesToConferences < ActiveRecord::Migration[5.2]
   def change
-    add_column :conferences, :allowed_event_types, :string, default: Event::TYPES.join(';')
+    add_column :conferences, :allowed_event_types, :string,
+      default: %w(lecture workshop podium lightning_talk meeting film concert djset performance other).join(';')
   end
 end
