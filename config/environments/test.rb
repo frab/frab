2d66c4183208a11d28b3f1f41686388841361d94
eladeyal-intel <elad.eyal@intel.com>
2020-03-02 20:38:31 +0200

    remove deprecation message from paper-trail

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,5 +44,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.paper_trail.enabled = false
+  config.after_initialize do
+    PaperTrail.enabled = false
+  end
 end
