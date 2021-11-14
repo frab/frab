67408e8e6bdf5ade82d80b2ce193fcc912af0d57
Mario Manno <mario.manno@gmail.com>
2021-11-14 16:01:24 +0100

    Comment on flaky test

--- test/features/ldap_test.rb
+++ test/features/ldap_test.rb
@@ -1,5 +1,8 @@
 require 'test_helper'
 
+# TODO This test uses a remote service and is flaky.
+# Fails with: `Authentication failure! ldap_error: Net::LDAP::Error, Connection
+# timed out - user specified timeout`
 class LdapTest < FeatureTest
   LOGIN='tesla'     # see https://www.forumsys.com/category/tutorials/integration-how-to/
   PASSWORD='password'
@@ -9,43 +12,43 @@ class LdapTest < FeatureTest
     assert User.where(email: EMAIL).blank?
   end
 
-  def connect_with_ldap(login, pwd)
+  def connect_with_ldap(login, pass)
     visit '/'
-   
+
     click_on 'Log-in'
-   
+
     click_on 'Sign in with free testing server at ldap.forumsys.com'
-   
+
     fill_in 'Login:', with: login
-    fill_in 'Password:', with: pwd
+    fill_in 'Password:', with: pass
     click_on 'Sign In'
-   
+
     assert_content page, 'Successfully authenticated'
     assert User.where(email: EMAIL).any?
-    
+
     click_on 'Logout'
   end
 
   test 'can sign up and sign in with LDAP' do
-    connect_with_ldap(LOGIN,PASSWORD) # for new user
-    connect_with_ldap(LOGIN,PASSWORD) # for existing user
-    connect_with_ldap(EMAIL,PASSWORD) # using e-mail instead of login field
+    connect_with_ldap(LOGIN, PASSWORD) # for new user
+    connect_with_ldap(LOGIN, PASSWORD) # for existing user
+    connect_with_ldap(EMAIL, PASSWORD) # using e-mail instead of login field
   end
 
   test 'rejects wrong password' do
     visit '/'
-   
+
     click_on 'Log-in'
-   
+
     click_on 'Sign in with free testing server at ldap.forumsys.com'
-   
+
     fill_in 'Login:', with: 'wrong'
     fill_in 'Password:', with: 'wrong'
     click_on 'Sign In'
-   
+
     assert_content page, 'Authentication failed'
   end
-  
+
   teardown do
     User.where(email: EMAIL).destroy_all
   end
