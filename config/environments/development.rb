87c10264daf5acce799fe383c969b1d7d545e9e6
Mario Manno <mario.manno@gmail.com>
2025-08-07 22:11:09 +0200

    Import conferences

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,8 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # needed for import controller
+    config.cache_store = :memory_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
